7997b426221b87fab3b7d86d9f3499593c15845b
Jackson Matheson <jackson.matheson@statseeker.com>
2019-08-05 09:38:01 +1000

    Fix spelling mistake in pytest.

--- pytest/test_main.py
+++ pytest/test_main.py
@@ -7,7 +7,7 @@ from main import hello_world
 class TestHelloWorld(unittest.TestCase):
 
     def test_hello_world(self):
-        self.assertEqual(hello_world(), 'Hello, World!', "Shoudl Return 'Hello, World!'")
+        self.assertEqual(hello_world(), 'Hello, World!', "Should Return 'Hello, World!'")
 
 if __name__ == '__main__':
     unittest.main()
